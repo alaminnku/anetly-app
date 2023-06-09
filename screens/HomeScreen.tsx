d5d15588117c29aa8b372053ef91a3d28e2bd954
Alamin Shaikh <hello@octib.com>
2023-06-09 11:20:26 +0600

    Search created

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,19 @@
 import { useNavigation } from "@react-navigation/native";
 import { useLayoutEffect } from "react";
-import { View, SafeAreaView, Image, StyleSheet, Text } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+} from "react-native";
+import {
+  ChevronDownIcon,
+  UserIcon,
+  AdjustmentsHorizontalIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -13,14 +26,35 @@ export default function HomeScreen() {
   }, []);
 
   return (
-    <SafeAreaView style={styles.header}>
-      <View style={styles.logo}>
-        <Image style={styles.image} source={require("../assets/logo.avif")} />
+    <SafeAreaView style={{ backgroundColor: "white" }}>
+      {/* Header */}
+      <View style={styles.header}>
+        <View style={styles.logo_and_title}>
+          <Image style={styles.logo} source={require("../assets/logo.avif")} />
+
+          <View>
+            <Text style={styles.delivery}>Deliver now</Text>
+            <Text style={styles.location}>
+              Current location
+              <ChevronDownIcon size={20} color="#00ccbb" />
+            </Text>
+          </View>
+        </View>
+
+        <UserIcon size={35} color="#00ccbb" />
       </View>
 
-      <View>
-        <Text style={styles.delivery}>Deliver now</Text>
-        <Text style={styles.location}>Current location</Text>
+      {/* Search */}
+      <View style={styles.search}>
+        <View style={styles.search_input}>
+          <MagnifyingGlassIcon color="gray" />
+          <TextInput
+            placeholder="Restaurant and cuisine"
+            keyboardType="default"
+          />
+        </View>
+
+        <AdjustmentsHorizontalIcon color="#00ccbb" />
       </View>
     </SafeAreaView>
   );
@@ -28,20 +62,25 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   header: {
-    display: "flex",
     flexDirection: "row",
     alignItems: "center",
+    paddingLeft: 15,
+    paddingRight: 15,
+    marginBottom: 15,
+    backgroundColor: "white",
+    justifyContent: "space-between",
   },
 
-  image: {
-    height: 50,
-    width: 50,
-    borderRadius: 100,
-    backgroundColor: "#d4d2d2",
+  logo_and_title: {
+    flexDirection: "row",
   },
 
   logo: {
+    height: 50,
+    width: 50,
     marginRight: 10,
+    borderRadius: 100,
+    backgroundColor: "#d4d2d2",
   },
 
   delivery: {
@@ -53,5 +92,21 @@ const styles = StyleSheet.create({
   location: {
     fontWeight: "bold",
     fontSize: 20,
+  },
+
+  search: {
+    paddingLeft: 15,
+    paddingRight: 15,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  search_input: {
+    flex: 1,
+    flexDirection: "row",
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 10,
+    backgroundColor: "#e6e5e5",
   },
 });
